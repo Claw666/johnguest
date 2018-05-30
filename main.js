47bec31da591acf5f2eac3e086aff07040f5d01b
Csongor Kocsis <csongor@uwork.co.uk>
2018-05-30 14:43:03 +0100

    changed address

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: {lat: 51.4142542, lng: -0.7803967}
+    zoom: 16,
+    center: {lat: 51.5099544, lng: -0.4680009}
   });
 
   var trafficLayer = new google.maps.TrafficLayer();
